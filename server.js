79b49fafaa5b1ba3d7abd2db5472a3cfdd168350
dannabelleli <belleli.danna@gmail.com>
2023-06-21 17:39:33 -0700

    heroku commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
-const e = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
